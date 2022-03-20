25309b8820f4cbde32989923451b5ca8a98e9db1
ZL84 <as@63ru.net>
2022-03-20 20:40:14 +0400

    Notification added

---
+++ app/src/main/java/com/project/tape/Playable.java
@@ -0,0 +1,8 @@
+package com.project.tape;
+
+public interface Playable {
+    void onTrackPrevious();
+    void onTrackNext();
+    void onTrackPlay();
+    void onTrackPause();
+}
---
+++ app/src/main/java/com/project/tape/Services/NotificationActionService.java
@@ -0,0 +1,13 @@
+package com.project.tape.Services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationActionService extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.sendBroadcast(new Intent("SONGS_SONGS")
+                .putExtra("actionName", intent.getAction()));
+    }
+}
